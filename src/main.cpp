e6286f38469274fdd369c811f5f6dbdda09ab42b
guillermo <gllrmoliva42@gmail.com>
2025-04-19 20:05:00 -0400

    Pruebas de complejidad temporal

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ int main()
 {
     std::cout << "Inicio el test" << std::endl;
 
-    test_complexity(bruteforceMinDist, "test3.csv", "brute_force", 8, 512, 1, 100);
-    test_complexity(divideconquerMinDist, "test3.csv", "divide_n_conquer", 8, 512, 1, 100);
+    test_complexity(bruteforceMinDist, "test.csv", "brute_force", 8, 512, 1, 20);
+    test_complexity(divideconquerMinDist, "test.csv", "divide_n_conquer", 8, 512, 1, 20);
 
     std::cout << "Finalizo el test" << std::endl;
 
